da3638532cb483d1524fbe6ab0a2fc5349a05d02
cygig <59651200+cygig@users.noreply.github.com>
2024-02-23 16:33:04 +0800

    v0.3.0

--- src/AlmostRandom.h
+++ src/AlmostRandom.h
@@ -18,27 +18,43 @@ class AlmostRandom{
   public:
     AlmostRandom();
     byte getRandomByte();
+    byte getLastRandomByte();
+
     int16_t getRandomInt();
     uint16_t getRandomUInt();
+    int32_t getRandomLong();
+    uint32_t getRandomULong();
+    float getRandomFloat();
+
 
     void enableRanalog();
     void disableRanalog();
-    byte setRanalog(unsigned int pin);
+    byte setRanalog(byte analogPin);
+    byte setRanalog(byte analogPin0, byte analogPin1, 
+                    byte analogPin2, byte analogPin3,
+                    byte analogPin4, byte analogPin5, 
+                    byte analogPin6, byte analogPin7);
     byte getRanalog();
+    byte getLastRanalog();
 
     void enableRamdom();
     void disableRamdom();
     byte setRamdom(byte* ramStart, byte* ramEnd);
     byte getRamdom();
+    byte getLastRamdom();
 
     void enableRanclock();
     void disableRanclock();
     byte setRanclock(byte* clockAddressA, byte* clockAddressB);
     byte getRanclock();
+    byte getLastRanclock();
     
 
   private: 
      byte randomByte;
+     byte ranalogByte;
+     byte ramdomByte;
+     byte ranclockByte;
      
      struct Flags
      {
@@ -46,6 +62,12 @@ class AlmostRandom{
       bool isEnabledRamdom : 1;
       bool isEnabledRanclock : 1;
      } flags;
+
+    byte analogPins[8];
+
+    byte* ramStart, ramEnd;
+
+    byte* clockAddressA, clockAddressB;
 
 
 
